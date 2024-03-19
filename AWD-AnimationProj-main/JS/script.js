1de8d81461727b8d9a2ccad6a22cc61c671a7567
heykyy <112928981+heykyy@users.noreply.github.com>
2024-03-19 20:58:13 +0800

    Delete AWD-AnimationProj-main directory

--- AWD-AnimationProj-main/JS/script.js
+++
@@ -1,6 +0,0 @@
-const mouseEvent = document.documentElement.style;
-
-mouseEvent.addEventListener('mousemove', (e) => {
-    mouseEvent.setProperty('--x', e.clientX + 'px');
-    mouseEvent.setProperty('--y', e.clientY + 'px');
-});
